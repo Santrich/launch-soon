6595ba902c7736aedb7bc8316f5691a28850ef9a
Luis Santrich <luis.santrich@gmail.com>
2016-02-12 09:02:45 +0100

    First changes to the main page

--- config/initializers/launch_soon.rb
+++ config/initializers/launch_soon.rb
@@ -8,16 +8,17 @@ MAILCHIMP_LIST_ID = 'LIST_ID'
 SITE_DOMAIN = 'DOMAIN.TLD'
 
 # Title tag for the site:
-SITE_TITLE = 'Title'
+# SITE_TITLE = 'Title'
+SITE_TITLE = 'Muy Pronto...'
 
 # Keywords for the site:
-SITE_KEYWORDS = 'key, words'
+SITE_KEYWORDS = 'Vainilla Store, Store, Vainilla, Joyas, Accesorios, accesorios para mujer, Dama, Mujer, Caballero, Accesorios Dama, Joyas Dama'
 
 # Description for the site, also used in Tweet so keep it short:
-SITE_DESCRIPTION = 'DESCRIPTION'
+SITE_DESCRIPTION = 'Joyas y accesorios para Dama'
 
 # Blurb about site, displayed on main page:
-SITE_BLURB = 'BLURB'
+SITE_BLURB = 'Vainilla Store'
 
 # Google Analytics code for the site:
 SITE_GOOGLE_ANALYTICS = 'UA-#######-##'
